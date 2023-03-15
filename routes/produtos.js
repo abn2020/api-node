a3d9fbbd3dc6fc1b4a72bdb1f57fd15df872782b
Abner Martins <abner.09.martins@gmail.com>
2023-03-15 09:33:25 -0300

    add repo api node js for pedidos and produtos

---
+++ routes/produtos.js
@@ -0,0 +1,168 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('../mysql').pool;
+
+router.get('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if(error){ return res.status(500).send({ error: error })}
+        conn.query(
+            'SELECT * from produtos;',
+            (error, result, field) => {
+                conn.release();
+                
+                if(error){ return res.status(500).send({ error: error, response: null })}
+                const response = {
+                    quantidade: result.length,
+                    produtos: result.map(prod => {
+                        return {
+                            id_produto: prod.id_produto,
+                            nome: prod.nome,
+                            preco: prod.preco,
+                            request: {
+                                tipo: 'GET',
+                                descricao: 'Retorna todos os produtos',
+                                url: 'http://localhost:5000/produtos/' + prod.id_produto
+                            }
+                        }
+                    })
+                }
+                res.status(200).send(response);
+            }
+        )
+    })
+});
+
+router.post('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if(error){ return res.status(500).send({ error: error })}
+        conn.query(
+            'INSERT INTO produtos (nome, preco) VALUES (?, ?)',
+            [req.body.nome, req.body.preco],
+            (error, result, field) => {
+                conn.release();
+                
+                if(error){ return res.status(500).send({ error: error, response: null })}
+                const response = {
+                    mensagem: 'Produto inserido com sucesso',
+                    produtoCriado: {
+                        id_produto: result.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco
+                    },
+                    request: {
+                        tipo: 'POST',
+                        descricao: 'Insere um produto',
+                        url: 'http://localhost:5000/produtos'
+                    }
+                }
+                res.status(201).send(response);
+            }
+        );
+    });
+});
+
+router.get('/:id_produto', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if(error){ return res.status(500).send({ error: error })}
+        conn.query(
+            'SELECT * from produtos WHERE id_produto = ?;',
+            [req.params.id_produto],
+            (error, result, field) => {
+                conn.release();
+                
+                if(error){ return res.status(500).send({ error: error, response: null })}
+                
+                if(result.length == 0){
+                    return res.status(404).send({
+                        mensagem: "Não foi encontrado nenhum produto"
+                    })
+                }
+                const response = {
+                    produto: {
+                        id_produto: result[0].id_produto,
+                        nome: result[0].nome,
+                        preco: result[0].preco
+
+                    },
+                    request: {
+                        tipo: 'GET',
+                        descricao: 'Retorna os detalhes de um produto',
+                        url: 'http://localhost:5000/produtos'
+                    }
+                }
+
+                res.status(200).send(response);
+            }
+        )
+    })
+});
+
+router.patch('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if(error){ return res.status(500).send({ error: error })}
+        conn.query(
+            `UPDATE produtos
+                SET nome        = ?,
+                    preco       = ? 
+               WHERE id_produto = ?`,
+            [
+                req.body.nome,
+                req.body.preco,
+                req.body.id_produto
+            ],
+            (error, result, field) => {
+                conn.release();
+                
+                if(error){ return res.status(500).send({ error: error, response: null })}
+
+                const response = {
+                    mensagem: 'Produto atualizado com sucesso',
+                    produtoAtualizado: {
+                        id_produto: req.body.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco
+                    },
+                    request: {
+                        tipo: 'GET',
+                        descricao: 'Retorna um produto',
+                        url: 'http://localhost:5000/produtos/' + req.body.id_produto
+                    }
+                }
+
+                res.status(202).send(response);
+            }
+        );
+    });
+});
+
+router.delete('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if(error){ return res.status(500).send({ error: error })}
+        conn.query(
+            `DELETE FROM produtos WHERE id_produto = ?`,
+            [req.body.id_produto],
+            (error, resultado, field) => {
+                conn.release();
+                
+                if(error){ return res.status(500).send({ error: error, response: null })}
+
+                const response = {
+                    mensagem: 'Produto removido com sucesso',
+                    request: {
+                        tipo: 'POST',
+                        descricao: 'Insere um produto',
+                        url: 'http://localhost:5000/produtos/' + req.body.id_produto,
+                        body: {
+                            nome: 'String',
+                            preco: 'number'
+                        }
+                    }
+                }
+
+                res.status(202).send(response);
+            }
+        );
+    });
+});
+
+module.exports = router;
